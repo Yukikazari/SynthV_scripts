5efbce63cf521c170629872149f81903bc099f93
Yukikazari <52287858+Yukikazari@users.noreply.github.com>
2020-08-17 22:55:15 +0900

    Update seek.lua

--- seek.lua
+++ seek.lua
@@ -3,39 +3,73 @@ function getClientInfo()
       name = "seek",
       category = "Play",
       author = "Yukikazari",
-      versionNumber = 1,
+      versionNumber = 2,
       minEditorVersion = 65540
     }
 end
 
 function readfile()
-    f = io.open("seek.txt", "r")
+    local f = io.open("seek.txt", "r")
+    local a = {}
     for line in f:lines() do
-        st = line
+        a[#a + 1] = line
     end
-    return st
+
+    if #a ~= 2 then
+        a[2] = a[1]
+    end
+
+    return a
+end
+
+function convstarttime(prop, splaystart)
+    local bplaystart = TimeAxis:getBlickFromSeconds(splaystart)
+    local range = viewrange[2] - viewrange[1]
+    local res = bplaystart - range * prop
+    return res
+end
+
+function convprop(splaystart)
+    local bplaystart = TimeAxis:getBlickFromSeconds(splaystart)
+    local range = viewrange[2] - viewrange[1]
+    local res = (bplaystart - viewrange[1]) / range
+    return res
 end
 
 function main()
-    local st = 0
-    local stat, ret = pcall(readfile)
-    if stat then
-        st = ret
-    end
+    local pb = SV:getPlayback()
+    local status = pb:getStatus()
 
-    pb = SV:getPlayback()
-    status = pb:getStatus()
+    local mv = SV:getMainEditor()
+    local mvcs = mv:getNavigation()
+    viewrange = mvcs:getTimeViewRange()
+
+    local pj = SV:getProject()
+    TimeAxis = pj:getTimeAxis()
 
     if status == "stopped" then
-        local sttm = pb:getPlayhead()
+        local playstart = pb:getPlayhead()
+        local prop = convprop(playstart)
+
         f = io.open("seek.txt", "w")
-        f:write(sttm)
+        f:write(playstart.."\n")
+        f:write(prop)
         f:close()
 
         pb:play()
     else
-        pb:seek(st)
+        local playstart = 0
+        local screenstart = 0
+        local stat, ret = pcall(readfile)
+        if stat then
+            playstart = ret[1]
+            screenstart = convstarttime(ret[2], playstart)
+        end
+
+        pb:seek(playstart)
         pb:pause()
+
+        mvcs:setTimeLeft(screenstart)
     end
     SV:finish()
 end
